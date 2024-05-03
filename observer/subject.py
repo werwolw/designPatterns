06d106c481ac28352d0ed966a951c57e0bf6689d
Konstantin Shiryaev <knshiryaev@avito.ru>
2024-05-03 19:44:24 +0500

    add observer pattern general

---
+++ observer/subject.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from observer.observer import Observer
+
+
+class Subject(ABC):
+    @abstractmethod
+    def register_observer(self, obs: Observer):
+        pass
+
+    @abstractmethod
+    def remove_observer(self, obs: Observer):
+        pass
+
+    @abstractmethod
+    def notify_observers(self):
+        pass
